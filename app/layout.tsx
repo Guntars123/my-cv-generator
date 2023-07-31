baca057b29acf55cc487bd1efceff06b5814f3e9
Guntars <gvaivods@gmail.com>
2023-07-31 19:43:28 +0300

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,10 +45,10 @@ const Navigation = () => {
               <a href="/">Home</a>
             </li>
             <li>
-              <a href="/about">About</a>
+              <a href="/">About</a>
             </li>
             <li>
-              <a href="/contact">Contact</a>
+              <a href="/">Contact</a>
             </li>
           </ul>
         </div>
